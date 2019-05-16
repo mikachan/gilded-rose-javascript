a226d948d88e10ada2b2fdc155348c86a6d3a5a0
Sarah <sarah@sekai.co.uk>
2019-05-16 22:18:10 +0100

    Use arrow functions in tests

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,11 +1,11 @@
-describe("Gilded Rose", function() {
+describe("Gilded Rose", () => {
     var items;
 
-    beforeEach(function() {
+    beforeEach(() => {
         items = [];
     });
 
-    it("should lower the sell_in and quality values by 1 for a default item", function() {
+    it("should lower the sell_in and quality values by 1 for a default item", () => {
         items.push(new Item("+5 Dexterity Vest", 10, 20));
 
         update_quality(items);
