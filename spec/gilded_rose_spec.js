be468ef3b88fc91c56b8d4faa72c06ca7d28c331
Sarah <sarah@sekai.co.uk>
2019-05-16 22:15:25 +0100

    Added initial test

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,7 +1,16 @@
 describe("Gilded Rose", function() {
+    var items;
 
-  it("should do something", function() {
-    update_quality();
-  });
+    beforeEach(function() {
+        items = [];
+    });
 
+    it("should lower the sell_in and quality values by 1 for a default item", function() {
+        items.push(new Item("+5 Dexterity Vest", 10, 20));
+
+        update_quality(items);
+
+        expect(items[0].sell_in).toBe(9);
+        expect(items[0].quality).toBe(19);
+    });
 });
