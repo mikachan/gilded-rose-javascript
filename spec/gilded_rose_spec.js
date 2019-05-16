02a8ed9670653961a2d61c2d03e9c4a8c521ab80
Sarah <sarah@sekai.co.uk>
2019-05-16 22:38:04 +0100

    Added tests for all spec rules from README

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -5,12 +5,107 @@ describe("Gilded Rose", () => {
         items = [];
     });
 
-    it("should lower the sell_in and quality values by 1 for a default item", () => {
+    it("should decrease the sell_in and quality values by 1 for a default item", () => {
         items.push(new Item("+5 Dexterity Vest", 10, 20));
 
         update_quality(items);
 
         expect(items[0].sell_in).toBe(9);
         expect(items[0].quality).toBe(19);
+    });
+
+    it("should decrease quality twice as fast if sell_in is less than 0", () => {
+        items.push(new Item("+5 Dexterity Vest", -1, 20));
+
+        update_quality(items);
+
+        expect(items[0].sell_in).toBe(-2);
+        expect(items[0].quality).toBe(18);
+    });
+
+    it("should never let the quality value be negative", () => {
+        items.push(new Item("+5 Dexterity Vest", 10, 0));
+
+        update_quality(items);
+
+        expect(items[0].sell_in).toBe(9);
+        expect(items[0].quality).toBe(0);
+    });
+
+    it("should increase the quality of Aged Brie the older it gets", () => {
+        items.push(new Item("Aged Brie", 1, 0));
+
+        update_quality(items);
+
+        expect(items[0].sell_in).toBe(0);
+        expect(items[0].quality).toBe(1);
+    });
+
+    it("should never let the quality of an item go above 50", () => {
+        items.push(new Item("Aged Brie", -50, 50));
+
+        update_quality(items);
+
+        expect(items[0].sell_in).toBe(-51);
+        expect(items[0].quality).toBe(50);
+    });
+
+    it("should never decrease the sell_in or quality values of Sulfuras", () => {
+        items.push(new Item("Sulfuras, Hand of Ragnaros", 80, 80));
+
+        update_quality(items);
+
+        expect(items[0].sell_in).toBe(80);
+        expect(items[0].quality).toBe(80);
+    });
+
+    it("should increase the quality of Backstage Passes the older it gets", () => {
+        items.push(
+            new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20)
+        );
+
+        update_quality(items);
+
+        expect(items[0].sell_in).toBe(14);
+        expect(items[0].quality).toBe(21);
+    });
+
+    it("should increase the quality of Backstage Passes by 2 when there are 10 days or less", () => {
+        items.push(
+            new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 7, 21)
+        );
+
+        update_quality(items);
+
+        expect(items[0].sell_in).toBe(9);
+        expect(items[0].quality).toBe(22);
+        expect(items[1].sell_in).toBe(6);
+        expect(items[1].quality).toBe(23);
+    });
+
+    it("should increase the quality of Backstage Passes by 3 when there are 5 days or less", () => {
+        items.push(
+            new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 2, 25)
+        );
+
+        update_quality(items);
+
+        expect(items[0].sell_in).toBe(4);
+        expect(items[0].quality).toBe(23);
+        expect(items[1].sell_in).toBe(1);
+        expect(items[1].quality).toBe(28);
+    });
+
+    it("should decrease the quality of Backstage Passes to 0 after the concert", () => {
+        items.push(
+            new Item("Backstage passes to a TAFKAL80ETC concert", -1, 20)
+        );
+
+        update_quality(items);
+
+        expect(items[0].sell_in).toBe(-2);
+        expect(items[0].quality).toBe(0);
     });
 });
